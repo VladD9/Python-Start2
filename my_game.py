5be8ea36333b5fd2b4d531bd88213f93cea1ff7a
VladD9 <147932519+VladD9@users.noreply.github.com>
2024-02-10 19:37:22 +0200

    Update my_game.py

--- my_game.py
+++ my_game.py
@@ -4,23 +4,49 @@ from PyQt5 import uic
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 
-lines = list(open("base.txt","r"))
-random_line = random.choice(lines)
-letters = list(random_line)
-
-
-
-
 
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('design.ui',self)
-        self.lines = list(open("base.txt","r"))
-        self.random_line = random.choice(lines)
-        self.letters = list(random_line)
-    def start_game(self):
-          
+        self.lines = list(open("base.txt","r",encoding="utf-8"))
+        self.random_line = random.choice(self.lines)
+        self.letters = list(self.random_line)
+
+        self.word_1 = list[0]
+        self.word_2 = list[1]
+        self.word_3 = list[2]
+        self.word_4 = list[3]
+        self.word_5 = list[4]
+
+        self.label_1.setText(str(self.word_1))
+        self.label_1.setVisible(False)
+        self.label_2.setText(str(self.word_2))
+        self.label_2.setVisible(False)
+        self.label_3.setText(str(self.word_3))
+        self.label_3.setVisible(False)
+        self.label_4.setText(str(self.word_4))
+        self.label_4.setVisible(False)
+        self.label_5.setText(str(self.word_5))
+        self.label_5.setVisible(False)
+
+        self.pushButton.clicked.connect(self.liter_1)
+        
+    def liter_1(self):
+        if "a" in list:
+            if "а" == self.word_1:
+                self.label_1.setVisible(True)
+            if "а" == list[1]:
+                self.label_2.setVisible(True)
+            if "а" == list[2]:
+                self.label_3.setVisible(True)
+            if "а" == list[3]:
+                self.label_4.setVisible(True)
+            if "а" == list[4]:
+                self.label_5.setVisible(True)
+        else:
+            life -= 1
+      
 
     
         
